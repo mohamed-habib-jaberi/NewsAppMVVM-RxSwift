552bafaf73c6beb99e3d7a2546c57a37c54454d7
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-07 14:04:07 +0200

    Setting Up the User Interface

---
+++ NewsAppMVVM+RxSwift/Controllers/NewsTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  NewsTableViewController.swift
+//  NewsAppMVVM+RxSwift
+//
+//  Created by mohamed  habib on 07/08/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class NewsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+       
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+//
+//        // Configure the cell...
+//
+//        return cell
+//    }
+//    
+
+
+
+}
